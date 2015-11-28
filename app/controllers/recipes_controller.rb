fa29b1263e327a0b108176ee086fe7758731ab59
wbucko <bucko.wojciech@gmail.com>
2015-11-28 17:30:53 +0000

    Added pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
     
    def index
-      @recipes = Recipe.all
+      @recipes = Recipe.paginate(page: params[:page], per_page: 4)
+      
    end
    
    def show
@@ -34,6 +35,18 @@ class RecipesController < ApplicationController
          redirect_to recipe_path(@recipe)
       else
          render :edit
+      end
+   end
+   
+   def like
+      @recipe = Recipe.find(params[:id])
+      like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+      if like.valid?
+         flash[:success] = "Your selection was succesful"
+         redirect_to :back
+      else
+         flash[:danger] = "You can only like/dislike a recipe once"
+         redirect_to :back
       end
    end
    
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'    
   get '/home', to: 'pages#home'
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
   
 end
